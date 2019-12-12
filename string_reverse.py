638111beaec3469866d935c2856e163efb6264db
Umeshjagtap15 <umeshjagtapk@gmail.com>
2019-12-12 12:48:35 +0530

    Learnini string reverse function

---
+++ string_reverse.py
@@ -0,0 +1,28 @@
+
+def reverse(string):
+    reverse_String = ""
+    for i in string:
+        reverse_String = i+reverse_String
+        print("String reverse is ", reverse_String)
+
+reverse("Sahoo")
+
+
+def reverse(s):
+  str = ""
+  for i in s:
+    str = i + str
+  return str
+
+
+
+def triangle(string):
+    reverse_String = ""
+    for i in string:
+        reverse_String = i+reverse_String
+        print(reverse_String)
+
+reverse("Sahoo")
+
+triangle("*****")
+
